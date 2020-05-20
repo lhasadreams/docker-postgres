1f7d88d5ac3dd8e955e013b6eaf77d4a8cd4743c
Tom Robinson-Gore <trg@chef.io>
2020-05-20 17:09:47 +0100

    Enforce control postgres-05

--- postgres-tests/controls/example.rb
+++ postgres-tests/controls/example.rb
@@ -4,6 +4,6 @@ require_controls 'postgres-baseline' do
   control 'postgres-02'
   control 'postgres-03'
   control 'postgres-04'
-  #control 'postgres-05'
+  control 'postgres-05'
   control 'postgres-09'
 end
